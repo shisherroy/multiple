faae9c330fbcf6ccf0160b8ce1cb39b0ff81d7f6
SHISHER <107603374+shisherroy@users.noreply.github.com>
2022-06-30 10:25:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('.one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+   
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.two').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:15,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
